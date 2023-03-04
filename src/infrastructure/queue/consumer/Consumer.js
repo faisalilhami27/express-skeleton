8f03c22d8ae709a754bd0df71e68697a1e02fbb0
Muhamad Faisal Ilhami Akbar <faisal.ilhami2797@gmail.com>
2023-03-04 18:16:15 +0700

    add retry in kafka

--- src/infrastructure/queue/consumer/Consumer.js
+++ src/infrastructure/queue/consumer/Consumer.js
@@ -17,12 +17,30 @@ class Consumer {
     this.consumer = this.kafka.consumer({groupId: constant.kafka.KAFKA_CLIENT_ID});
   }
 
+  async processMessage(message, callback) {
+    try {
+      callback(message);
+    } catch (error) {
+      console.error('An error occurred: ' + error);
+      this.consumer.pause();
+      setTimeout(() => {
+        this.consumer.resume();
+      }, 3000);
+      throw error;
+    }
+  };
+
   async consumeMessage(topic, callback) {
     await this.consumer.connect();
     await this.consumer.subscribe({topic});
     await this.consumer.run({
-      eachMessage: async ({message}) => {
-        callback(message);
+      eachMessage: async ({ topic, partition, message }) => {
+        try {
+          await this.processMessage(message, callback);
+          await this.consumer.commitOffsets([{ topic, partition, offset: message.offset }]);
+        } catch (error) {
+          console.error(error);
+        }
       },
     });
   }
--- src/infrastructure/queue/producer/Producer.js
+++ src/infrastructure/queue/producer/Producer.js
@@ -7,20 +7,38 @@ class Producer {
     this.kafka = new Kafka({
       clientId: constant.kafka.KAFKA_CLIENT_ID,
       brokers: utils.splitKafkaBroker(constant.kafka.KAFKA_BROKERS),
+      retry: {
+        maxRetryTime: 5000,
+        initialRetryTime: 3000,
+        retries: parseInt(constant.kafka.KAFKA_MAX_RETRY),
+        restartOnFailure: async () => true
+      }
     });
     this.producer = this.kafka.producer();
   }
 
   async sendMessage(topic, message) {
+    let retries = 0;
+    let delay = 100;
+    const maxRetry = parseInt(constant.kafka.KAFKA_MAX_RETRY);
     await this.producer.connect();
-    await this.producer.send({
-      topic,
-      messages: [
-        {
-          value: JSON.stringify(message),
-        },
-      ],
-    });
+    while (retries < maxRetry) {
+      try {
+        await this.producer.send({
+          topic,
+          messages: [
+            { value: JSON.stringify(message) }
+          ]
+        });
+      } catch (error) {
+        console.log('Error in sending message to kafka: ' + error);
+        if (retries < maxRetry) {
+          retries++;
+          await new Promise(resolve => setTimeout(resolve, delay));
+          delay *= 2;
+        }
+      }
+    }
   }
 }
 
